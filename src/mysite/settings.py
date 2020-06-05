de3c61dbb87d374ee4779f1e66d285e883a36252
Evan Tey <evantey14@gmail.com>
2020-06-05 11:26:14 -0400

    Disable l10n i18n and set tz to UTC

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -107,16 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'America/Los_Angeles'
-
-USE_I18N = True
-
-USE_L10N = True
+USE_I18N = False
 
 USE_TZ = True
+TIME_ZONE = 'UTC'
 
 
 # Static files (CSS, JavaScript, Images)
