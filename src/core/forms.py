d99ea40e5654d2f8e41ee4d2ed57f4599a3ee0e2
Evan Tey <evantey14@gmail.com>
2020-06-16 10:21:32 -0400

    Make form fields not required bc lazy

--- src/core/forms.py
+++ src/core/forms.py
@@ -5,18 +5,19 @@ from django.db import transaction
 from .models import Student, ESPUser
 
 class ESPSignUpForm(UserCreationForm):
-    phone_number = forms.CharField(max_length=20, label="Phone Number") #TODO there's a phone number field we can add with pip
+    phone_number = forms.CharField(max_length=20, label="Phone Number", required=False) #TODO there's a phone number field we can add with pip
     pronouns = forms.CharField(max_length=40, required=False)
-    city = forms.CharField(max_length=200)
-    state = forms.CharField(max_length=200, label="State (Leave blank if outside the US)")
-    country = forms.CharField(max_length=200)
+    city = forms.CharField(max_length=200, required=False)
+    state = forms.CharField(max_length=200, label="State (Leave blank if outside the US)", required=False)
+    country = forms.CharField(max_length=200, required=False)
 
     class Meta(UserCreationForm.Meta):
         model = ESPUser
+        fields = ("username", "email", "password1", "password2", "first_name", "last_name",)
 
 class StudentSignUpForm(ESPSignUpForm):
-    dob = forms.DateField(label="Date of Birth")
-    grad_year = forms.IntegerField(label="Graduation Year")
+    dob = forms.DateField(label="Date of Birth", required=False)
+    grad_year = forms.IntegerField(label="Graduation Year", required=False)
     school = forms.CharField(max_length=200, required=False)    
 
     @transaction.atomic
