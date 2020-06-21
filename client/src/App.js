a6e6dbdec10e8976c81b5b228393a84ba0538833
Sophie Mori <ksmori14@gmail.com>
2020-06-21 01:00:00 -0400

    Add simple homepage and navigation menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+// import { Button } from "react-bulma-components";
+
+import "react-bulma-components/dist/react-bulma-components.min.css";
+
+import Nav from "./Nav.js";
+
+let Home = () => (
+  // TODO: why doesn't this bulma class work?
+  <section className="pt-6" style={{ paddingTop: "3rem" }}>
+    <div className="container">
+      <div className="columns">
+        <div className="column">
+          <img
+            className="level-item"
+            src="https://qph.fs.quoracdn.net/main-qimg-338d75dbb172519677a52c5d7db1192d"
+          />{" "}
+        </div>
+        <div className="column">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel
+          nunc ut enim sagittis interdum. Vestibulum dapibus urna eu neque
+          luctus maximus. Sed eu cursus nunc, id imperdiet lorem. Praesent arcu
+          felis, varius sed nibh eu, vehicula molestie nunc. Morbi ac arcu id
+          ante porttitor tempor. Suspendisse mollis tellus ipsum. Morbi pretium
+          mi sit amet eros consequat, ac ornare tortor lobortis. Morbi
+          scelerisque sit amet dolor ac ullamcorper. Curabitur eget ullamcorper
+          ligula. Fusce bibendum bibendum massa. Nunc non luctus est. Phasellus
+          ante massa, condimentum vitae suscipit eget, cursus at nunc. Cras
+          porta, lorem et condimentum dapibus, velit ex laoreet purus, sit amet
+          blandit mi urna eu arcu. Aliquam porta felis et felis porta, et
+          fermentum purus laoreet. Pellentesque faucibus odio sit amet lectus
+          finibus luctus. Pellentesque nec felis blandit, vulputate nisi ac,
+          porttitor justo. Cras at urna sit amet metus efficitur varius ac sit
+          amet libero. Mauris dictum lacus quis velit finibus, eget tincidunt
+          erat feugiat.
+        </div>
+        <div className="column">Fish! Learn! Teach!</div>
+      </div>
+    </div>
+  </section>
+);
+let Dash = () => <div>Dash</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Nav />
+      <main>
+        <Router>
+          <Home path="/" />
+          <Dash path="dashboard" />
+        </Router>
+      </main>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>
+            <strong>ESPIDER</strong> by espider-dev@mit.edu. The source code is
+            licensed{" "}
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+            The website content is licensed{" "}
+            {/* TODO: What is this, actually? */}
+            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+              CC BY NC SA 4.0
+            </a>
+            .
+          </p>
+        </div>
+      </footer>
+    </React.Fragment>
   );
 }
 
---
+++ client/src/Nav.js
@@ -0,0 +1,76 @@
+import React from "react";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileOpen: false,
+    };
+  }
+
+  toggleMobileMenu = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
+  render() {
+    const { mobileOpen } = this.state;
+    return (
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            MIT ESP
+          </a>
+
+          <a
+            role="button"
+            className={`navbar-burger burger ${mobileOpen ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbar"
+            onClick={this.toggleMobileMenu}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div
+          id="navbar"
+          className={`navbar-menu ${mobileOpen ? "is-active" : ""}`}
+        >
+          {/* <div className="navbar-start"></div> */}
+
+          <div className="navbar-end">
+            <a className="navbar-item">Learn</a>
+
+            <a className="navbar-item">Teach</a>
+
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">Programs</a>
+
+              <div className="navbar-dropdown">
+                <a className="navbar-item">Splash</a>
+                <a className="navbar-item">Spark</a>
+                <a className="navbar-item">HSSP</a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item">Firestorm</a>
+              </div>
+            </div>
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light">Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
