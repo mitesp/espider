367096d26d7ed5440a6f1e74459fcc00f0d7f3d8
Sophie Mori <ksmori14@gmail.com>
2020-06-24 19:39:24 -0400

    Generate footer links

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -1,6 +1,45 @@
 import React from "react";
 
+const generateLink = (href, text) => (
+  <a className="has-text-link-dark" href={href}>
+    {text}
+  </a>
+);
+
+const generateLinksList = (linksInfo) => (
+  <ul>
+    {linksInfo.map((x) => (
+      <li>{generateLink(x[0], x[1])}</li>
+    ))}
+  </ul>
+);
+
 export default function () {
+  const programs = [
+    ["/splash", "Splash", "9–12"],
+    ["/spark", "Spark", "7–8"],
+    ["/hssp#spring", "Spring HSSP", "7–12"],
+    ["/hssp#summer", "Summer HSSP", "7–12"],
+    ["/cascade", "Cascade", "9–10"],
+  ];
+  const mitPrograms = [
+    ["/firehose", "Firehose"],
+    ["/firestorm", "Firestorm"],
+    ["/splashformit", "Splash for MIT"],
+  ];
+  const about = [
+    ["/history", "History"],
+    ["/team", "Team"],
+    ["/values", "Values"],
+  ];
+  const involved = [
+    ["/learn", "Learn"],
+    ["/teach", "Teach"],
+    ["/volunteer", "Volunteer"],
+    ["/outreach", "Spread the word"],
+    ["/join", "Join us"],
+  ];
+
   return (
     <footer className="footer has-background-success-light">
       <div className="container">
@@ -15,115 +54,39 @@ export default function () {
             </p>
             <br />
             <p>
-              We host educational events for local middle and high school students and for the MIT
-              community. Our mission is to spread the joy of learning and teaching, and to help
-              people share their knowledge, passions, and hobbies with others.
+              We host educational events for local middle and high school
+              students and for the MIT community. Our mission is to spread the
+              joy of learning and teaching, and to help people share their
+              knowledge, passions, and hobbies with others.
             </p>
             <br />
-            <p className="has-text-weight-bold">Learn anything, teach anything, do anything!</p>
+            <p className="has-text-weight-bold">
+              Learn anything, teach anything, do anything!
+            </p>
           </div>
           <div className="column is-1"></div>
           <div className="column" style={{ lineHeight: "2rem" }}>
             <h2 className="is-size-6 pt-0 has-text-weight-bold">Programs</h2>
             <ul>
-              <li>
-                <a className="has-text-link-dark" href="/splash">
-                  Splash
-                </a>
-                <small className="pl-2 has-text-grey-dark">9–12</small>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/spark">
-                  Spark
-                </a>
-                <small className="pl-2 has-text-grey-dark">7–8</small>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/hssp#spring">
-                  Spring HSSP
-                </a>
-                <small className="pl-2 has-text-grey-dark">7–12</small>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/hssp#summer">
-                  Summer HSSP
-                </a>
-                <small className="pl-2 has-text-grey-dark">7–12</small>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/cascade">
-                  Cascade
-                </a>
-                <small className="pl-2 has-text-grey-dark">9–10</small>
-              </li>
+              {programs.map((x) => (
+                <li>
+                  {generateLink(x[0], x[1])}
+                  <small className="pl-2 has-text-grey-dark">{x[2]}</small>
+                </li>
+              ))}
             </ul>
-            <h2 className="is-size-6 pt-0 has-text-weight-bold">Programs for MIT</h2>
-            <ul>
-              <li>
-                <a className="has-text-link-dark" href="/firehose">
-                  Firehose
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/firestorm">
-                  Firestorm
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/splashformit">
-                  Splash for MIT
-                </a>
-              </li>
-            </ul>
+            <h2 className="is-size-6 pt-0 has-text-weight-bold">
+              Programs for MIT
+            </h2>
+            {generateLinksList(mitPrograms)}
           </div>
           <div className="column" style={{ lineHeight: "2rem" }}>
             <h2 className="is-size-6 has-text-weight-bold">About Us</h2>
-            <ul>
-              <li>
-                <a className="has-text-link-dark" href="/history">
-                  History
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/team">
-                  Team
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/values">
-                  Values
-                </a>
-              </li>
-            </ul>
-
-            <h2 className="is-size-6 has-text-weight-bold">Get Involved</h2>
-            <ul>
-              <li>
-                <a className="has-text-link-dark" href="/learn">
-                  Learn
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/teach">
-                  Teach
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/volunteer">
-                  Volunteer
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/outreach">
-                  Spread the word
-                </a>
-              </li>
-              <li>
-                <a className="has-text-link-dark" href="/join">
-                  Join us
-                </a>
-              </li>
-            </ul>
+            {generateLinksList(about)}
+            <h2 className="is-size-6 has-text-weight-bold">
+              Get Involved
+            </h2>{" "}
+            {generateLinksList(involved)}
           </div>
         </div>
         <hr />
@@ -133,10 +96,14 @@ export default function () {
             espider-dev@mit.edu
           </a>
           . The source code is licensed{" "}
-          <a className="has-text-link-dark" href="http://opensource.org/licenses/mit-license.php">
+          <a
+            className="has-text-link-dark"
+            href="http://opensource.org/licenses/mit-license.php"
+          >
             MIT
           </a>
-          . The website content is licensed {/* TODO: What is this, actually? */}
+          . The website content is licensed{" "}
+          {/* TODO: What is this, actually? */}
           <a
             className="has-text-link-dark"
             href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
