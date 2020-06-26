fc32b7e27d8e916607162e8ace05aae3feb70674
Mayukha Vadari <mvadari@gmail.com>
2020-06-25 21:44:39 -0700

    Remove django from django URLs

--- server/config/urls.py
+++ server/config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("token-auth/", obtain_jwt_token),
-    path("django/", include("core.urls")),
+    path("", include("core.urls")),
 ]
