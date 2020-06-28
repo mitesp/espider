35e22776ba580c3aa2b8b3ac15442708b5f1cf75
Evan Tey <evantey14@gmail.com>
2020-06-27 23:07:34 -0400

    Rename state and props types

--- client/src/accounts/SignupPage.tsx
+++ client/src/accounts/SignupPage.tsx
@@ -3,18 +3,18 @@ import SignupForm from "./SignupForm";
 import { UserState } from "../App";
 import { SignupType } from "./types";
 
-type SignupProps = {
+type Props = {
   onLogin: (data: UserState) => void;
   loggedIn: boolean;
   username: string;
 };
 
-type SignupState = {
+type State = {
   signupType: SignupType;
 };
 
-class SignupPage extends Component<SignupProps, SignupState> {
-  constructor(props: SignupProps) {
+class SignupPage extends Component<Props, State> {
+  constructor(props: Props) {
     super(props);
     this.state = {
       signupType: SignupType.Student,
