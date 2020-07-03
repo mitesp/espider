ddcd693db321a9a841d02503e987399c228796fd
Mayukha Vadari <mvadari@gmail.com>
2020-07-02 21:41:24 -0700

    Clean up admin panel
      - Display more + more useful information
      - Change all relevant studentreg objects' statuses
          when Program status is updated

--- server/core/admin.py
+++ server/core/admin.py
@@ -25,13 +25,33 @@ class TeacherInline(admin.StackedInline):
 class ESPUserAdmin(admin.ModelAdmin):
     fields = (
         "username",
+        ("first_name", "last_name"),
         "email",
-        "first_name",
-        "last_name",
     )
     list_display = ("username", "email", "first_name", "last_name", "is_student", "is_teacher")
-    search_fields = ("username", "email")
+    search_fields = ("username", "email", "first_name", "last_name")
     inlines = [StudentInline, TeacherInline]
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "program", "capacity")
+    search_fields = ("title", "id")
+    readonly_fields = ("id", "num_students")
+    fields = ("id", "title", ("capacity", "num_students"), "program", "description")
+
+    # TODO figure out how to add field to add/remove teachers registered for the program
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    fields = (("name", "edition"), "student_reg_open", "student_reg_status", "teacher_reg_open")
+
+    def save_model(self, request, obj, form, change):
+        if "student_reg_status" in form.changed_data:
+            studentregs = StudentRegistration.objects.filter(program=obj)
+            studentregs.update(reg_status=obj.student_reg_status)
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(StudentRegistration)
@@ -40,6 +60,7 @@ class StudentRegistrationAdmin(admin.ModelAdmin):
         "id",
         "student",
         "program",
+        "reg_status",
         "update_profile_check",
         "emergency_info_check",
         "medliab_check",
@@ -55,11 +76,6 @@ class TeacherRegistrationAdmin(admin.ModelAdmin):
     list_display = ("teacher", "program", "update_profile_check")
     search_fields = ("teacher", "program")
 
-
-admin.site.register(Class)
-admin.site.register(StudentProfile)
-admin.site.register(TeacherProfile)
-admin.site.register(Program)
 
 admin.site.register(TeacherClassRegistration)
 admin.site.register(StudentClassRegistration)
