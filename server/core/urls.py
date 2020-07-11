131619306bc2dee63adfcca1ea30aa7b3db230ba
Evan Tey <evantey14@gmail.com>
2020-07-11 19:23:15 -0400

    Remove Django templates, views, and forms (#18)
    * Remove Django templates, views, and forms
    
    * Add TODO note for broken teacher api endpoint

--- server/core/urls.py
+++ server/core/urls.py
@@ -6,59 +6,11 @@ from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 router = routers.DefaultRouter()
+# TODO: This is currently broken -- will get fixed when we do teacher stuff
 router.register("dashboard/teacher/", views.TeacherProgramViewSet)
 
 app_name = "core"
 urlpatterns = [
-    # general pages
-    # path("index", views.index, name="index"),
-    # path("studentdashboard", views.studentdashboard, name="studentdashboard"),
-    # path("teacherdashboard", views.teacherdashboard, name="teacherdashboard"),
-    # accounts pages
-    path(
-        "accounts/",
-        include(
-            [
-                path("", include("django.contrib.auth.urls")),
-                path("signup/", views.SignUpPageView.as_view(), name="signup"),
-                path("signup/student/", views.StudentSignUpView.as_view(), name="student_signup",),
-                path("signup/teacher/", views.TeacherSignUpView.as_view(), name="teacher_signup",),
-                path("signup/parent/", views.ParentSignUpView.as_view(), name="parent_signup",),
-                path(
-                    "signup/educator/", views.EducatorSignUpView.as_view(), name="educator_signup",
-                ),
-                path("profile/", views.index),  # TODO make this redirect to the home page somehow
-            ]
-        ),
-    ),
-    # student registration
-    # path(
-    #     "<program>/<edition>/studentprofile/",
-    #     views.StudentProfileView.as_view(),
-    #     name="studentprofile",
-    # ),
-    # path("<program>/<edition>/emergency_info/", views.emergency_info, name="emergency_info"),
-    # path("<program>/<edition>/medliab/", views.medliab, name="medliab"),
-    # path("<program>/<edition>/waiver/", views.waiver, name="waiver"),
-    path("<program>/<edition>/studentclassreg/", views.studentclassreg, name="studentclassreg"),
-    # path("<program>/<edition>/studentreg/", views.studentregdashboard,name="studentregdashboard"),
-    path("<program>/<edition>/studentclasses/", views.studentclasses, name="studentclasses"),
-    # teacher registration
-    path(
-        "<program>/<edition>/teacherprofile/",
-        views.TeacherProfileView.as_view(),
-        name="teacherprofile",
-    ),
-    path(
-        "<program>/<edition>/teacherclassreg/",
-        views.TeacherRegistrationView.as_view(),
-        name="teacherclassreg",
-    ),
-    path("<program>/<edition>/teacherreg/", views.teacherregdashboard, name="teacherregdashboard"),
-    # admin view pages
-    path("<program>/<edition>/students/", views.StudentsView.as_view(), name="students"),
-    path("<program>/<edition>/classes/", views.ClassesView.as_view(), name="classes"),
-    # api paths
     # TODO: these paths are kinda messy -- we should read about API design and clean this up.
     path("api/", include(router.urls)),
     path("api/<program>/<edition>/catalog/", views.ClassCatalog.as_view()),
