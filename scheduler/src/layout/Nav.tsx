4fb4bfae6e495571903da1842d1cc39e45760497
Mayukha Vadari <mvadari@gmail.com>
2020-07-15 22:02:07 -0700

    Get initial app working

--- scheduler/src/layout/Nav.tsx
+++ scheduler/src/layout/Nav.tsx
@@ -7,9 +7,7 @@ type Props = {
   logout: () => void;
 };
 
-type State = {
-  mobileOpen: boolean;
-};
+type State = {};
 
 class Nav extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -19,16 +17,9 @@ class Nav extends React.Component<Props, State> {
     };
   }
 
-  toggleMobileMenu = () => {
-    this.setState({ mobileOpen: !this.state.mobileOpen });
-  };
-
   loggedOutView = () => {
     return (
       <div className="buttons">
-        <a className="button is-primary" href="/signup">
-          <strong>Sign up</strong>
-        </a>
         <a className="button is-light" href="/login">
           Log In
         </a>
@@ -38,35 +29,17 @@ class Nav extends React.Component<Props, State> {
 
   loggedInView = () => {
     return (
-      <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link has-text-weight-bold" href="/dashboard">
-          {this.props.username}
-        </a>
+      <div>
+        <h1 className="navbar-item has-text-weight-bold">{this.props.username}</h1>
 
-        <div className="navbar-dropdown is-right">
-          <a className="navbar-item" href="/dashboard">
-            <span className="icon pr-3">
-              <i className="fas fa-home"></i>
-            </span>
-            Dashboard
-          </a>
-          <a className="navbar-item" href="/profile">
-            <span className="icon pr-3">
-              <i className="fas fa-user"></i>
-            </span>
-            Profile
-          </a>
-          <hr className="navbar-divider"></hr>
-          <a className="navbar-item" href="/logout" onClick={this.props.logout}>
-            Log Out
-          </a>
-        </div>
+        <a className="navbar-item" href="/logout" onClick={this.props.logout}>
+          Log Out
+        </a>
       </div>
     );
   };
 
   render() {
-    const { mobileOpen } = this.state;
     return (
       <nav
         className="navbar has-background-success-light"
@@ -78,40 +51,12 @@ class Nav extends React.Component<Props, State> {
             <a className="navbar-item has-text-weight-bold" href="/">
               MIT ESP
             </a>
-
-            <button
-              type="button"
-              className={`button-reset navbar-burger burger ${mobileOpen ? "is-active" : ""}`}
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="navbar"
-              onClick={this.toggleMobileMenu}
-            >
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </button>
           </div>
+          {/* <div className="navbar-start"></div> */}
 
-          <div id="navbar" className={`navbar-menu ${mobileOpen ? "is-active" : ""}`}>
-            {/* <div className="navbar-start"></div> */}
-
-            <div className="navbar-end">
-              <a className="navbar-item has-text-weight-bold" href="/learn">
-                Learn
-              </a>
-
-              <a className="navbar-item has-text-weight-bold" href="/teach">
-                Teach
-              </a>
-
-              <a className="navbar-item has-text-weight-bold" href="/aboutus">
-                About Us
-              </a>
-            </div>
-            <div className="navbar-item">
-              {this.props.loggedIn ? this.loggedInView() : this.loggedOutView()}
-            </div>
+          <div className="navbar-end"> </div>
+          <div className="navbar-item">
+            {this.props.loggedIn ? this.loggedInView() : this.loggedOutView()}
           </div>
         </div>
       </nav>
