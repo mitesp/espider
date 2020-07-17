697d6ff108ba7666be88a16d12803536a946d65b
Mayukha Vadari <mvadari@gmail.com>
2020-07-16 17:14:53 -0700

    Make app even simpler

--- scheduler/src/App.tsx
+++ scheduler/src/App.tsx
@@ -6,39 +6,20 @@ import "./App.sass";
 import Home from "./Home";
 import LoginPage from "./accounts/LoginPage";
 
-export type UserState = {
-  loggedIn: boolean;
-  username: string;
-  isStudent: boolean;
-  isTeacher: boolean;
-};
-
-type State = UserState;
-
 class App extends Component<{}, State> {
   constructor(props) {
     super(props);
-    this.state = {
-      loggedIn: localStorage.getItem("token") ? true : false,
-      username: "",
-      isStudent: false,
-      isTeacher: false,
-    };
+    this.state = {};
   }
-
-  componentDidMount() {}
 
   login = (data: UserState) => {
     this.setState(data);
-    //TODO: manually calling component did mount?
-    this.componentDidMount();
   };
 
   logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("refresh");
     delete axiosInstance.defaults.headers.common["Authorization"];
-    this.setState({ loggedIn: false, username: "", isStudent: false, isTeacher: false });
   };
 
   render() {
@@ -47,12 +28,7 @@ class App extends Component<{}, State> {
         <main className="px-3 py-5">
           <Router>
             <Home path="/" />
-            <LoginPage
-              path="login"
-              onLogin={this.login}
-              loggedIn={this.state.loggedIn}
-              username={this.state.username}
-            />
+            <LoginPage path="login" />
           </Router>
         </main>
       </React.Fragment>
