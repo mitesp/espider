4b2eb0237e81ddc245979aa3ebf61184c4e4390f
Evan Tey <evantey14@gmail.com>
2020-07-17 00:51:38 -0400

    more changes

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -76,8 +76,6 @@ REST_FRAMEWORK = {
         # "rest_framework.authentication.BasicAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
 }
 
 CORS_ORIGIN_WHITELIST = (
