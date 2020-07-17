92897f769289a3d02916a3a97babeeb130a82c1a
Mayukha Vadari <mvadari@gmail.com>
2020-07-16 18:37:30 -0700

    Change Scheduler to class

--- scheduler/src/Scheduler.tsx
+++ scheduler/src/Scheduler.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function () {
-  return (
-    <div className="container content">
-      <div className="columns">
-        <div className="column has-text-centered has-background-success-light">
-          Insert scheduler here
+export default class Scheduler extends Component<{}, {}> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container content">
+        <div className="columns">
+          <div className="column has-text-centered has-background-success-light">
+            Insert scheduler here
+          </div>
+          <div className="column is-3 has-text-centered">Insert class list here</div>
         </div>
-        <div className="column is-3 has-text-centered">Insert class list here</div>
       </div>
-    </div>
-  );
+    );
+  }
 }
