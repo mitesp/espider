a9da1d2fe0df7cfce45ede07dbcd8bf4d80a21ef
Mayukha Vadari <mvadari@gmail.com>
2020-07-17 11:30:19 -0700

    Fix typo

--- client/src/helperTextFunctions.tsx
+++ client/src/helperTextFunctions.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 function renderTextInSection(displayedText: string, centered = false) {
-  return (
-    <h3 className={"is-size-5" + (centered ? " has has-text-centered" : "")}>{displayedText}</h3>
-  );
+  return <h3 className={"is-size-5" + (centered ? " has-text-centered" : "")}>{displayedText}</h3>;
 }
 
 function renderLinkedText(displayedText: string, url: string) {
