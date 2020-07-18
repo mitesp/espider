776629c7e20c86492652690b75647319a16c297c
Mayukha Vadari <mvadari@gmail.com>
2020-07-18 16:29:51 -0700

    Simplify tests

--- server/core/tests/models/program.py
+++ server/core/tests/models/program.py
@@ -4,18 +4,10 @@ from django.test import TestCase
 
 class ProgramTests(TestCase):
     def setUp(self):
-        Program.objects.create(name="Droplet", edition="3030")
-        Program.objects.create(name="Slosh", edition="420")
-
-        # open student registration for one object
-        program = Program.objects.get(name="Droplet", edition="3030")
-        program.student_reg_open = True
-        program.save()
-
-        # make one program a past program
-        program = Program.objects.get(name="Slosh", edition="420")
-        program.student_reg_status = RegStatusOptions.POST_PROGRAM
-        program.save()
+        Program.objects.create(name="Droplet", edition="3030", student_reg_open=True)
+        Program.objects.create(
+            name="Slosh", edition="420", student_reg_status=RegStatusOptions.POST_PROGRAM
+        )
 
     def test_url(self):
         program = Program.objects.get(name="Droplet", edition="3030")
@@ -23,19 +15,13 @@ class ProgramTests(TestCase):
 
     def test_open_student_programs(self):
         open_student_programs = Program.get_open_student_programs()
-        self.assertEquals(len(open_student_programs), 1)
-
-        open_student_program = open_student_programs[0]
         expected_program = Program.objects.get(name="Droplet", edition="3030")
-        self.assertEquals(open_student_program, expected_program)
+        self.assertListEqual(list(open_student_programs), [expected_program])
 
     def test_active_programs(self):
         active_programs = Program.get_active_programs()
-        self.assertEquals(len(active_programs), 1)
-
-        active_program = active_programs[0]
         expected_program = Program.objects.get(name="Droplet", edition="3030")
-        self.assertEquals(active_program, expected_program)
+        self.assertListEqual(list(active_programs), [expected_program])
 
 
 class TimeslotTests(TestCase):
