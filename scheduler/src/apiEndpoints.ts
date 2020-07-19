4d0163b7718cd6e76bfd76be3aa502f9589d5fe3
Mayukha Vadari <mvadari@gmail.com>
2020-07-19 16:33:20 -0700

    Switch API export method

--- scheduler/src/apiEndpoints.ts
+++ scheduler/src/apiEndpoints.ts
@@ -1,16 +1,8 @@
 // Accounts
-const loginEndpoint = "/token/";
-const tokenRefreshEndpoint = "/token/refresh/";
+export const loginEndpoint = "/token/";
+export const tokenRefreshEndpoint = "/token/refresh/";
 
 // Program-specific (these requires /program/edition/ before)
-const timeslotEndpoint = "timeslots/";
-const classroomEndpoint = "classrooms/";
-const classesEndpoint = "catalog/";
-
-export {
-  loginEndpoint,
-  tokenRefreshEndpoint,
-  timeslotEndpoint,
-  classroomEndpoint,
-  classesEndpoint,
-};
+export const timeslotEndpoint = "timeslots/";
+export const classroomEndpoint = "classrooms/";
+export const classesEndpoint = "catalog/";
