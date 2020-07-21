69aa0bee379e534d30b7049d10493ecf2ab4f1a2
Mayukha Vadari <mvadari@gmail.com>
2020-07-21 15:30:34 -0700

    Fix tooltips

--- scheduler/src/DisplayedClass.tsx
+++ scheduler/src/DisplayedClass.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
 import { useDrag } from "react-dnd";
 
 import { Section } from "./types";
@@ -16,22 +17,37 @@ export default function DisplayedSection(props: Props) {
   });
 
   return (
-    <div
-      ref={drag}
-      className="box"
-      key={props.section.id}
-      data-tip
-      data-for={"class-data" + props.section.id}
-    >
-      <div className="content">
-        <p>
-          <i>
-            <strong>{props.section.clazz}</strong> - {props.section.name}
-          </i>
-          <br />
-          Other relevant information
-        </p>
+    <div>
+      <div
+        ref={drag}
+        className="box"
+        key={props.section.id}
+        data-tip
+        data-for={"classData-" + props.section.id}
+      >
+        <div className="content">
+          <p>
+            <i>
+              <strong>{props.section.clazz}</strong> - {props.section.name}
+            </i>
+            <br />
+            Other relevant information
+          </p>
+        </div>
       </div>
+      <ReactTooltip
+        id={"classData-" + props.section.id}
+        key={"classData-" + props.section.id}
+        place="right"
+        type="info"
+        effect="solid"
+      >
+        <span>
+          {props.section.name}
+          <br />
+          Show class information here
+        </span>
+      </ReactTooltip>
     </div>
   );
 }
--- scheduler/src/ScheduledClass.tsx
+++ scheduler/src/ScheduledClass.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTooltip from "react-tooltip";
 import { useDrag } from "react-dnd";
 
 import { Section } from "./types";
@@ -15,5 +16,24 @@ export default function ScheduledClass(props: Props) {
     }),
   });
 
-  return <span ref={drag}>{`class ${props.section.clazz} sec ${props.section.number}`}</span>;
+  return (
+    <div>
+      <span ref={drag} data-tip data-for={"classData-" + props.section.id}>
+        {`class ${props.section.clazz} sec ${props.section.number}`}
+      </span>
+      <ReactTooltip
+        id={"classData-" + props.section.id}
+        key={"classData-" + props.section.id}
+        place="right"
+        type="info"
+        effect="solid"
+      >
+        <span>
+          {props.section.name}
+          <br />
+          Show class information here
+        </span>
+      </ReactTooltip>
+    </div>
+  );
 }
